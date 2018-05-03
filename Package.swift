2b7c0ded4d6438216eab073e19b6946d09234960
Hulk <hulk104255@gmail.com>
2018-05-03 22:33:16 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/yoshiki/Curl.git", majorVersion: 0),
         .Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 12),
-        .Package(url: "https://github.com/Zewo/HTTPServer.git", majorVersion: 0, minor: 13),
         .Package(url: "https://github.com/ZewoGraveyard/Base64.git", majorVersion: 0, minor: 12),
     ],
     exclude: [ "EnvironmentTests" ]
