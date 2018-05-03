73084a6c50bf6993e79ea74bf98a59d8fa1df6d1
Hulk <hulk104255@gmail.com>
2018-05-03 22:56:28 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 let package = Package(
     name: "LINEBotAPI",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 12),
         .Package(url: "https://github.com/ZewoGraveyard/Base64.git", majorVersion: 0, minor: 12),
     ],
     exclude: [ "EnvironmentTests" ]
